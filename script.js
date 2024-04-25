c710726744f525f4f5a4a8ac1dd0b3c330e54182
bharagy <145537623+bharagy@users.noreply.github.com>
2024-04-25 16:44:22 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+
+function convert(){
+    var inputValue= document.getElementById
+    ('firstvalue').value;
+    var result=document.getElementById
+    ('secondvalue')
+    result.value= (inputValue*9/5)+32;
+}
